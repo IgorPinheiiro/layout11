ebc21db84f4b2310056baa6d49c4c223ee96b0ce
Igor Pinheiro <igorpinheiro976@gmail.com>
2024-06-09 20:07:46 -0300

    alterando server

--- src/main/resources/schema-postgresql.sql
+++ src/main/resources/schema-postgresql.sql
@@ -1,8 +1,5 @@
 CREATE TABLE IF NOT EXISTS aluno (
      id serial PRIMARY KEY,
      nome  varchar(50),
-     telefone   varchar(12),
-     idade int(2),
-     observacao varchar(50),
-     responsavel varchar(50)
+     cpf   varchar(11)
 );
