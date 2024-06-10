0ae039cee0507d063179b6335e984b5c42476926
Igor Pinheiro <igorpinheiro976@gmail.com>
2024-06-09 21:40:37 -0300

    fix alunoDAO

--- src/main/java/br/com/projeto/layout/model/AlunoDAO.java
+++ src/main/java/br/com/projeto/layout/model/AlunoDAO.java
@@ -6,7 +6,7 @@ import java.util.Map;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.dao.DataAccessException;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -25,25 +25,25 @@ public class AlunoDAO {
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
-    public void inserirAluno(Aluno aluno) {
-        String sql = "INSERT INTO aluno(nome, observacao, responsavel, idade, telefone) VALUES (?, ?, ?, ?, ?)";
-        Object[] params = new Object[5];
-        params[0] = aluno.getNome();
-        params[1] = aluno.getObservacao();
-        params[2] = aluno.getResponsavel();
-        params[3] = aluno.getIdade();
-        params[4] = aluno.getTelefone();
-        jdbcTemplate.update(sql, params);
-    }
-
     // public void inserirAluno(Aluno aluno) {
     //     String sql = "INSERT INTO aluno(nome, observacao, responsavel, idade, telefone) VALUES (?, ?, ?, ?, ?)";
-    //     try {
-    //         jdbcTemplate.update(sql, aluno.getNome(), aluno.getObservacao(), aluno.getResponsavel(), aluno.getIdade(), aluno.getTelefone());
-    //     } catch (DataAccessException e) {
-    //         e.printStackTrace();
-    //     }
+    //     Object[] params = new Object[5];
+    //     params[0] = aluno.getNome();
+    //     params[1] = aluno.getObservacao();
+    //     params[2] = aluno.getResponsavel();
+    //     params[3] = aluno.getIdade();
+    //     params[4] = aluno.getTelefone();
+    //     jdbcTemplate.update(sql, params);
     // }
+
+    public void inserirAluno(Aluno aluno) {
+        String sql = "INSERT INTO aluno(nome, observacao, responsavel, idade, telefone) VALUES (?, ?, ?, ?, ?)";
+        try {
+            jdbcTemplate.update(sql, aluno.getNome(), aluno.getObservacao(), aluno.getResponsavel(), aluno.getIdade(), aluno.getTelefone());
+        } catch (DataAccessException e) {
+            e.printStackTrace();
+        }
+    }
 
     public List<Map<String, Object>> listarAlunos() {
         String sql = "SELECT * FROM aluno";
