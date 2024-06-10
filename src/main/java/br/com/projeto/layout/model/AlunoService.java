696d618729bc8c734e2ab866396b4b6079cff083
Igor Pinheiro <igorpinheiro976@gmail.com>
2024-06-09 22:22:44 -0300

    @service

--- src/main/java/br/com/projeto/layout/model/AlunoService.java
+++ src/main/java/br/com/projeto/layout/model/AlunoService.java
@@ -4,8 +4,9 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-
+@Service
 public class AlunoService {
     @Autowired AlunoDAO cdao;
 
